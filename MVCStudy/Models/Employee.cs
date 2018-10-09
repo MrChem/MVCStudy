f811e3739ad3e93ed0eddb17f12874884772991b
MrChem <972572191@qq.com>
2018-10-09 23:11:30 +0800

    实验 13——添加服务器端验证

--- MVCStudy/Models/Employee.cs
+++ MVCStudy/Models/Employee.cs
@@ -11,7 +11,9 @@ namespace MVCStudy.Models
     {
         [Key]
         public int EmployeeId { get; set; }
+        [Required(ErrorMessage ="Enter the firstname")]
         public string FirstName { get; set; }
+        [StringLength(5,ErrorMessage = "Last Name length should not be greater than 5")]
         public string LastName { get; set; }
         public int Salary { get; set; }
     }
